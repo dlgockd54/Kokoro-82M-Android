f90183cbeae9da70fbe194a5387e74142c7b1c06
Hae Chang Lee <dlgockd45@gmail.com>
2025-03-29 15:56:34 +0900

    TTS 시작 버튼 클릭 시 TTS 시작

--- app/src/main/java/com/hclee/kokoro_82m_android/MainActivity.kt
+++ app/src/main/java/com/hclee/kokoro_82m_android/MainActivity.kt
@@ -4,44 +4,66 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import com.hclee.kokoro_82m_android.ui.theme.Kokoro82MAndroidTheme
+import com.hclee.kokoro_82m_android.ui.theme.TtsPerformer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val ttsPerformer = TtsPerformer()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Kokoro82MAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainContent(
+                    onTtsStartButtonClicked = {
+                        lifecycleScope.launch(Dispatchers.IO) {
+                            ttsPerformer.start(
+                                context = this@MainActivity,
+                            )
+                        }
+                    }
+                )
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun MainContent(
+    onTtsStartButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center,
+    ) {
+        Button(
+            onClick = onTtsStartButtonClicked,
+        ) {
+            Text("TTS 시작")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
+private fun MainContentPreview() {
     Kokoro82MAndroidTheme {
-        Greeting("Android")
+        MainContent(
+            onTtsStartButtonClicked = {},
+        )
     }
 }
